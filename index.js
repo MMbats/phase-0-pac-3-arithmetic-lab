1841c4e06e870350c9a6a71828338e61284e24e7
MMbats <petermbatia657@gmail.com>
2024-10-13 02:54:44 +0300

    done

---
+++ index.js
@@ -0,0 +1,40 @@
+// Function to add two numbers`
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Function to subtract two numbers
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Function to multiply two numbers
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  // Function to divide two numbers
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  // Function to increment a number
+  function increment(n) {
+    return n + 1;
+  }
+  
+  // Function to decrement a number
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  // Function to parse a string and convert it to an integer
+  function makeInt(string) {
+    return parseInt(string, 10);
+  }
+  
+  // Function to parse a string and convert it to a floating-point number
+  function preserveDecimal(string) {
+    return parseFloat(string);
+  }
+  
